91b15ae1c1b4f3411e29d8499c676146b3595584
Hemant Sharma <hemantsharma2894@gmail.com>
2016-12-08 19:14:08 +0530

    final

--- src/DapperExercise/Startup.cs
+++ src/DapperExercise/Startup.cs
@@ -29,6 +29,7 @@ namespace DapperExercise
         {
             // Add framework services.
             services.AddMvc();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +39,8 @@ namespace DapperExercise
             loggerFactory.AddDebug();
 
             app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUi();
         }
     }
 }
